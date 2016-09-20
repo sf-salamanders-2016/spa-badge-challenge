c76bbae1409420b28e12ad89a1e8b0ca41b0a06a
Jeff Roy & Tedmund Chua <pair+jeffreyroy.tchua93@devbootcamp.com>
2016-09-20 14:58:32 -0700

    Add voting functiopn

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
   loadStudents();
   loadBadges();
+  homeButtonListener();
+  voteListener();
 });
 
 
 
 var loadStudents = function() {
-    url = "http://sample-badges-api.herokuapp.com/teachers"
+    var url = "http://sample-badges-api.herokuapp.com/teachers"
     $.ajax({
       url: url,
       method: "get"
     })
     .done(function(response){
-      console.log(response)
         var context = {
           student: response
         };
@@ -20,7 +21,6 @@ var loadStudents = function() {
           var theTemplateScript = $("#student-template").html();
           var theTemplate = Handlebars.compile(theTemplateScript);
           var theCompiledHtml = theTemplate(context);
-          console.log(theCompiledHtml);
           $(document.body).append(theCompiledHtml);
 
         })
@@ -32,10 +32,10 @@ var loadStudents = function() {
 
 var loadBadges = function() {
   $("#index-page").on("click", ".student-link", function(event) {
-    console.log(event);
     event.preventDefault();
     var id = $(this).attr("id");
-    url = "http://sample-badges-api.herokuapp.com/teachers/" + id
+    var name = $("#" + id).html();
+    var url = "http://sample-badges-api.herokuapp.com/teachers/" + id
     $.ajax({
       url: url,
       method: "get"
@@ -43,15 +43,54 @@ var loadBadges = function() {
     .done(function(response){
         $("#student-container").hide();
         var context = {
+          name: name,
           badge: response
         };
         $(function() {
           var theTemplateScript = $("#badges-list").html();
           var theTemplate = Handlebars.compile(theTemplateScript);
           var theCompiledHtml = theTemplate(context);
-          console.log(theCompiledHtml);
           $(document.body).append(theCompiledHtml);
     })
   })
   })
+
+
+}
+
+var homeButtonListener = function() {
+  $("#index-page").on("click", "#home", function(event) {
+    event.preventDefault();
+    $(".show-user").hide();
+    $("#student-container").show();
+  })
+}
+
+var voteListener = function() {
+  $("#index-page").on("submit", ".vote-on form", function(event) {
+    event.preventDefault();
+    var vote = $(this).serialize();
+    var id = $(this).parent().parent().attr("id");
+    console.log(id);
+    var url = "http://sample-badges-api.herokuapp.com/badges/" + id
+    // $.ajax({
+    //   url: url,
+    //   method: "put"
+    // })
+    // .done(function(response){
+    //   console.log(response)
+
+        // var context = {
+        //   name: name,
+        //   badge: response
+        // };
+    //     $(function() {
+
+    // })
+  })
+})
+}
+
+var addBadge = function () {
+
 }
